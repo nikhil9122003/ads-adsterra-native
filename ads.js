94031b5437bce8719ec488c59f86142ff15d92ec
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 00:46:45 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,14 +1,19 @@
-// File: ads.js
 (function() {
+  var adDiv = document.createElement('div');
+  adDiv.id = "container-fe89cab288c7303a990fc20b03e35977";
+
+  // Append to a specific container instead of body
+  var target = document.getElementById("nativeAdHere");
+  if (target) {
+    target.appendChild(adDiv);
+  }
+
   var s = document.createElement('script');
   s.async = true;
   s.setAttribute('data-cfasync', 'false');
   s.src = "https://pl27266514.profitableratecpm.com/fe89cab288c7303a990fc20b03e35977/invoke.js";
 
-  document.body.appendChild(s);
-
-  // Optional: Create target div for widget if required
-  var adDiv = document.createElement('div');
-  adDiv.id = "container-fe89cab288c7303a990fc20b03e35977";
-  document.body.appendChild(adDiv);
+  if (target) {
+    target.appendChild(s);
+  }
 })();
