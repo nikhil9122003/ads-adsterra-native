e222682641fc1b2018cb652efc928d42d2b1e949
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 00:05:24 +0530

    Update and rename index.html to ads.js

---
+++ ads.js
@@ -0,0 +1,14 @@
+// File: ads.js
+(function() {
+  var s = document.createElement('script');
+  s.async = true;
+  s.setAttribute('data-cfasync', 'false');
+  s.src = "https://pl27266514.profitableratecpm.com/fe89cab288c7303a990fc20b03e35977/invoke.js";
+
+  document.body.appendChild(s);
+
+  // Optional: Create target div for widget if required
+  var adDiv = document.createElement('div');
+  adDiv.id = "container-fe89cab288c7303a990fc20b03e35977";
+  document.body.appendChild(adDiv);
+})();
